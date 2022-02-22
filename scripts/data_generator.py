1bd08e39c69160b4f77eb716053f3552b9edff00
Donatien Schmitz <donatien@schmitz.cc>
2022-02-22 15:05:15 +0000

    chunk data generation in 1gb blocks

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -8,7 +8,7 @@ import sys
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "k:s:m:", ["data_dir=", "keys=", "key_matching=", "size="])
+        opts, args = getopt.getopt(sys.argv[1:], "k:s:m:r:", ["data_dir=", "keys=", "key_matching=", "size=", 'row_size='])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
@@ -19,7 +19,8 @@ def main():
     keys = 10
     key_matching = 0.5
     dataset_size = 1 # In Gigabytes
-    data_dir = "/home/donatien/GEPICIAD/resource-estimator/sql_illustration/data/tables/"
+    row_size = 100
+    data_dir = "/home/donatien/GEPICIAD/resource-estimator/xp/sql_illustration/data/tables/"
 
     for o, a in opts:
         if o == "--data_dir":
@@ -29,19 +30,23 @@ def main():
         elif o in ("-m", "--key_matching"):
             key_matching = float(a)
         elif o in ("-s", "--size"):
-            dataset_size = float(a)
+            dataset_size = int(a)
+        elif o in ("-r", "--row_size"):
+            row_size = int(a)
         else:
             assert False, "unhandled option"
 
     pathlib.Path(data_dir).mkdir(exist_ok=True, parents=True)
     table1_row = "{}|AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n"
     table2_row = "{}|BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\n"
-    row_size = 100
     size_per_table = dataset_size * 1073741824 # in bytes
     rows_per_table = int(size_per_table / row_size)
 
     open(data_dir + "table_1.dat", "w").write(''.join(table1_row.format(i) for i in range(keys)))
-    open(data_dir + "table_2.dat", "w").write(''.join(table2_row.format(randint(0,int(keys / key_matching))) for i in range(rows_per_table)))
+    with open(data_dir + "table_2.dat", "w") as fd:
+        for i in range(dataset_size):
+            print("Writing {}/{}Gb".format(i+1, dataset_size))
+            fd.write(''.join(table2_row.format(randint(0, int(keys / key_matching))) for j in range(int(rows_per_table / dataset_size))))
 
 if __name__ == "__main__":
     main()
